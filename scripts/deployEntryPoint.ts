472fc069e403f7b8e3a903b1bed5368f7632f3e8
MetaXFuel <falkdolciy@rambler.ru>
2025-05-13 23:17:47 +0300

    Update deployEntryPoint.ts

--- scripts/deployEntryPoint.ts
+++ scripts/deployEntryPoint.ts
@@ -1,1 +1,18 @@
 
+import { ethers } from "hardhat";
+
+async function main() {
+    const [deployer] = await ethers.getSigners();
+
+    console.log("Deploying EntryPoint contract with the account:", deployer.address);
+
+    const EntryPoint = await ethers.getContractFactory("EntryPoint");
+    const entryPoint = await EntryPoint.deploy();
+
+    console.log("EntryPoint deployed to:", entryPoint.address);
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
