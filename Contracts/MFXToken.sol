ce98d158d7889673958a605a3f8e105414634c9d
MetaXFuel <falkdolciy@rambler.ru>
2025-05-13 23:12:25 +0300

    Update MFXToken.sol

--- Contracts/MFXToken.sol
+++ Contracts/MFXToken.sol
@@ -1,1 +1,13 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract GASXToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("GasX", "GASX") {
+        _mint(msg.sender, initialSupply);
+    }
+
+    // Optional: Add any extra methods (e.g., for staking or burning) here if needed
+}
