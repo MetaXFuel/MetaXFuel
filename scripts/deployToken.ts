cdb0fb23878887825f0f15985ea39bbad205693e
MetaXFuel <falkdolciy@rambler.ru>
2025-05-13 23:17:57 +0300

    Update deployToken.ts

--- scripts/deployToken.ts
+++ scripts/deployToken.ts
@@ -1,1 +1,19 @@
 
+import { ethers } from "hardhat";
+
+async function main() {
+    const [deployer] = await ethers.getSigners();
+    const initialSupply = ethers.utils.parseUnits("1000000", 18); // 1 million tokens
+
+    console.log("Deploying GASXToken with the account:", deployer.address);
+
+    const GASXToken = await ethers.getContractFactory("GASXToken");
+    const gasXToken = await GASXToken.deploy(initialSupply);
+
+    console.log("GASXToken deployed to:", gasXToken.address);
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
